ef7069af9a1575e86ff65b35aa38ff3e5cab01e0
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-12-03 21:40:52 +0200

    update

---
+++ Selecting Data from a Single Table/RowCounts.sql
@@ -0,0 +1,7 @@
+use employees;
+select count(*) from employees.departments;
+select count(*) from employees.dept_emp;
+select count(*) from employees.dept_manager;
+select count(*) from employees.employees;
+select count(*) from employees.salaries;
+select count(*) from employees.titles;
