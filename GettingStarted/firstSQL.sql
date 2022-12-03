aacb9d07a9d10342ccfaa3b1dc86f738688750a1
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-12-03 11:48:00 +0200

    First Assignement

---
+++ GettingStarted/firstSQL.sql
@@ -0,0 +1,9 @@
+-- DDL Data Dedinition Language
+CREATE TABLE tasks(
+tast_name VARCHAR(255),
+complete CHAR(1));
+
+-- DML Data Manipulation Language
+INSERT INTO tasks VALUES ('Study SQL', 'Y');
+
+SELECT * FROM tasks;
