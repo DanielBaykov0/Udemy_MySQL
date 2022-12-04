7a7d3679ee94404b6d8f75f23831f6a6db53d0ab
Daniel Baykov <99421433+DanielBaykov0@users.noreply.github.com>
2022-12-04 19:44:28 +0200

    update

---
+++ Selecting Data from a Single Table/WHEREwithORDERBY.sql
@@ -0,0 +1,59 @@
+use employees;
+
+SELECT 
+    *
+FROM
+    salaries;
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary ASC;
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC;
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY from_date , salary DESC;
+
+SELECT 
+    *
+FROM
+    employees;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name;
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Elvis'
+ORDER BY birth_date;
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Elvis'
+ORDER BY gender , birth_date DESC;
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Elvis'
+ORDER BY gender DESC , birth_date DESC;
